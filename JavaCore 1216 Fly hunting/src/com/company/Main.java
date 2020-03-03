864718cb87db4b09b7128326666df29ef6422d2c
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-03-03 09:32:52 +0200

    Add files via upload

---
+++ JavaCore 1216 Fly hunting/src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+/*
+
+1216 Fly hunting.
+Hunting fly
+Write your public Fly interface.
+Add two methods to it.
+
+Requirements:
+1. The Solution class must contain the Fly interface.
+2. Fly interface must be public.
+3. The Fly interface should contain two methods.
+4. The Fly interface must not have a parent.
+
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public interface Fly {
+        public void start();
+        public void stop();
+    }
+
+}
+
